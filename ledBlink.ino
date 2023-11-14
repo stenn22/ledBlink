d1a08dd62ff5776244c281135b767239269871ac
stenn22 <150845986+stenn22@users.noreply.github.com>
2023-11-14 13:49:09 -0500

    added second led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,6 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;
+int ledPin2 = 14;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT)
 }
 
 void loop() {
