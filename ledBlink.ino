34e8fb1e8bf00fca9e10c1ee6c284a5e5f3c7b9a
stenn22 <150845986+stenn22@users.noreply.github.com>
2023-11-14 13:55:07 -0500

    added code to turn on

--- ledBlink.ino
+++ ledBlink.ino
@@ -9,5 +9,12 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
+  delay(500);
 }
